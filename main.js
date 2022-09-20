4289d38e966bcbe7a3be04593ef37676c06e650b
d-reynolds3 <djreyn83@gmail.com>
2022-09-20 10:46:06 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,67 @@
+//Assignment 2
+
+//question 1
+let myNumber;
+
+//question 2
+myNumber = 5;
+
+//question 3
+let myOtherNumber;
+myOtherNumber = 5;
+
+//question 4
+let myTotal;
+mytotal = myNumber * myOtherNumber;
+
+//question 5
+let myName;
+myName = "David";
+
+//question 6
+let myOtherCombo = myNumber + myOtherNumber;
+
+//question 7
+let myRemainder = myNumber/5;
+
+//quetion 8 
+let i;
+i = 2;
+
+//question 9 
+let int;
+int = 4;
+
+//question 10
+let myBoolean;
+myBoolean = false;
+
+
+
+//question 1 and 2
+console.log(myNumber);
+
+//question 3
+console.log(myOtherNumber);
+
+//question 4
+console.log(myTotal);
+
+//question 5
+console.log(myName);
+
+//question 6 
+console.log(myOtherCombo);
+
+//question 7
+console.log(myRemainder);
+
+//question 8
+console.log(i);
+
+//question 9
+console.log(int);
+
+//question 10 
+console.log(myBoolean);
+
